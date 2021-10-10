98b3df0591daac760d6ec6a253e44a114293162d
SeaBassLab <70863556+SeaBassLab@users.noreply.github.com>
2021-10-10 16:17:52 -0300

    new fix

--- api/config.ts
+++ api/config.ts
@@ -34,7 +34,7 @@ export default {
 	TOKEN_SECRET: process.env.JWT_SECRET || 'TESTINMO',
 	LIC_PASSWORD: process.env.LIC_SECRET || 'TESTINMOLIC',
 	MONGO_URL: process.env.MONGO_URL || 'mongodb://localhost:27017/',
-	CLUSTER: 'mongodb+srv://monty:some_pass@cluster0.vzota.mongodb.net',
+	CLUSTER: 'mongodb+srv://SeaBass:seba 65501 mongoDB@cluster0.0nqlo.mongodb.net',
 
 	PERMISSIONS: permissions
 
--- index.ts
+++ index.ts
@@ -1,6 +1,14 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
+import 'dotenv/config'
 
-export default function (req: VercelRequest, res: VercelResponse) {
-  const { name = 'World' } = req.query;
-  res.send(`Hello ${name}!`);
-}
+import TYPES from './api/TYPES'
+import container from './api/inversify.config'
+
+import Appeable from './api/utils/Appeable'
+import RouterApp from './api/Presentation/Router/Controller'
+
+// const dot = require('dotenv').config({ path: './env' });
+
+const routerApp: RouterApp = new RouterApp()
+const app = container.get<Appeable>(TYPES.Appeable)
+
+app.run(routerApp);
